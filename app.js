81c595b77020d06757393b655ec8f37adbf1e5a7
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-05-21 14:54:16 +0430

    remove disabled code from app.js

--- app.js
+++ app.js
@@ -5,13 +5,4 @@ const express = require('express');
 const app = express();
 require('./startup/routes')(app);           //Setup APIs routes
 
-/*
-const port = config.get('port');            //Read port number from the config file
-const server = app.listen(port, () => { 
-    console.log(`start listening to port ${port} ...`) ;
-});
-
-//module.exports = server;
-*/
-
 module.exports = app;
